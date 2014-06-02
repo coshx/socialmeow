5d42d0133c9dc3d45078f0b84bd141da81aad8c8
Miša Mаkaröv <michael@michaels-MacBook-Air.local>
2014-06-02 13:44:09 -0700

    Improves statistics data view

--- app/controllers/api/dashboard_controller.rb
+++ app/controllers/api/dashboard_controller.rb
@@ -7,15 +7,16 @@ class Api::DashboardController < ApiController
     users.each do |u|
     	result = {}
 	    followed = u.accounts.order(:followed_date).where(followed: true, following: false)
+	    result[:name] = u.email
 	    result[:accounts] = u.accounts.where.not(parent: nil).count
 	    result[:accounts_followed] = followed.count
 	    result[:accounts_followed_back] = u.followed_back.count
 	    first_date = followed.first.followed_date
 	    last_date = followed.last.followed_date
-	    result[:days_of_word] = (last_date - first_date).to_i / 1.day
-	    if result[:days_of_word] > 0
-		    result[:followed_per_day] = result[:accounts_followed] / result[:days_of_word]
-		    result[:followed_back_per_day] = result[:accounts_followed_back] / result[:days_of_word]
+	    result[:days_of_work] = (last_date - first_date).to_i / 1.day
+	    if result[:days_of_work] > 0
+		    result[:followed_per_day] = result[:accounts_followed] / result[:days_of_work]
+		    result[:followed_back_per_day] = result[:accounts_followed_back] / result[:days_of_work]
 		end
 	    results << result
    	end
